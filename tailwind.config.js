c9013c4de319a71c29a54cfc2437d6ffd1790670
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-02-28 09:57:24 +0530

    For the large devices the navbar completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+
+    container: {
+      center: true,
+      padding: "1rem",
+      screens : {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl" : "1124px",
+      }
+    },
+
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"]
+      },
+      colors : {
+        "bookmark-purple" : "#5267DF",
+        "bookmark-red" : "#FA5959",
+        "bookmark-blue" : "#242A45",
+        "bookmark-gray" : "#9194A2",
+        "bookmark-white" : "#f7f7f7"
+      }
+    }
+    
   },
   plugins: [],
 }
